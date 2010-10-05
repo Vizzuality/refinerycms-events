0dc88d2bb72f29f031ba2f70c9316ab341fc793a
Fernando Blat <ferblape@gmail.com>
2010-10-05 16:35:49 +0200

    Added related events, close in time

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,13 @@ class Event < ActiveRecord::Base
   validates_presence_of :title
   validates_uniqueness_of :title
 
+  def similar(options = {})
+    default_options = {
+      :distance_in_days => 2,
+      :limit => 10
+    }
+    options = default_options.merge(options)
+    Event.limit(options[:limit]).where("id != #{self.id}").where("events.from >= '#{(self.from + options[:distance_in_days].days).to_s(:db)}'")
+  end
+
 end
